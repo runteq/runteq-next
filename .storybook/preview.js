58bb246661b7cd055fa8b6176b5bf98cf8a9ac8e
tsuchiya-yu2 <yuji2tsuchiya@gmail.com>
2022-09-04 18:20:33 +0900

    add/Storybookの設定を追加

---
+++ .storybook/preview.js
@@ -0,0 +1,52 @@
+import { addDecorator } from '@storybook/react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { theme } from '../themes'
+import * as NextImage from 'next/image'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetice Neus, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    transition: .25s;
+    color: #000000;
+  }
+`
+
+// Themeの適用
+addDecorator((story) => (
+  <ThemeProvider theme ={theme}>
+    <GlobalStyle />
+    {story()}
+  </ThemeProvider>
+))
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  consigurable: true,
+  value: (props) => typeof props.src === 'string' ? (
+    <OriginalnextImage {...props} unoptimized blurDataURL={props.src} />
+  ) : (
+    <OriginalNextImage {...props} unoptimized />
+  ),
+})
